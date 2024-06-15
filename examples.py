3154978cfabd1a067e7c5fc1e5040704403444ad
joshu <mail.joshuastanley@gmail.com>
2024-06-15 18:20:51 +0530

    Implement the complete and exit button !!

--- examples.py
+++ examples.py
@@ -1,18 +1,21 @@
-
 import FreeSimpleGUI as sg
 
-feet_label = sg.Text("Enter feet: ")
-feet_input = sg.Input()
+# Prepare the widgets for the left column
+left_column_content = [[sg.Text('Left 1')],
+                       [sg.Text('Left 2')]]
 
-inches_label = sg.Text("Enter inches: ")
-inches_input = sg.Input()
+# Prepare the widgets for the right column
+right_column_content = [[sg.Text('Right 1')],
+                        [sg.Text('Right 2')]]
 
-button = sg.Button("Convert")
+# Construct the Column widgets
+left_column = sg.Column(left_column_content)
+right_column = sg.Column(right_column_content)
 
-window = sg.Window("Convertor",
-                   layout=[[feet_label, feet_input],
-                           [inches_label, inches_input],
-                           [button]])
+# Construct the layout
+layout = [[left_column, right_column]]
 
+# Construct and display the window
+window = sg.Window('Columns', layout)
 window.read()
 window.close()
--- files/gui.py
+++ files/gui.py
@@ -7,22 +7,28 @@ add_button = sg.Button("Add") # this is a instance of add
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[35,10])
 edit_Button=sg.Button("edit")
+complete_Button=sg.Button("complete")
+exit_Button=sg.Button("exit")
+
+button_labels = ["close","apply"]
 
 # This is an instance of a window(this is father of all the arguments which contains the list)
 window = sg.Window('My To-Do app',
-                   layout=[[label], [input_box, add_button],[list_box,edit_Button]],
+                   layout=[[label], [input_box, add_button],
+                           [list_box,edit_Button,complete_Button],
+                           [exit_Button]],
                    font=('Helvetica', 15))
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
-    print(values['todos'])
+    print(1,event)
+    print(2,values)
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values = todos)
         case "edit":
             todos_to_edit = values['todos'][0]
             new_todo = values['todo']
@@ -32,7 +38,14 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "exit":
+            break
         case "sg.WIN_Closed":
             break
-
 window.close()
